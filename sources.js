6a285610a7b392bd72b380685ff33dbe7fae04ce
Hasan Burak Songur <hasanburaksongur@gmail.com>
2023-03-03 16:27:21 +0300

    create basic math operator functions

---
+++ sources.js
@@ -0,0 +1,47 @@
+let a = 5
+let b = 3
+function add(a,b) {
+    result = a + b;
+    console.log(result)
+    return result
+}
+add(a,b)
+
+function extract(a,b) {
+    result = a - b;
+    console.log(result)
+    return result
+}
+extract(a,b)
+
+function multiply(a,b) {
+    result = a*b;
+    console.log(result)
+    return result
+}
+multiply(a,b)
+function divide(a,b) {
+    result = a/b;
+    console.log(result)
+    return result
+}
+divide(a,b)
+function negative(a){
+    result = a*(-1);
+    console.log(result)
+    return result
+ }
+ negative(a)
+ function remainder(a,b) {
+    result = a%b;
+    console.log(result)
+    return result
+}
+remainder(a,b)
+
+function squareRoot(a) {
+    result = Math.sqrt(a)
+    console.log(result)
+    return result
+}
+squareRoot(a)
