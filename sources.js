6c617efd9ae6639cb9f8cfb0d1d711a58753c79c
Hasan Burak Songur <hasanburaksongur@gmail.com>
2023-03-09 18:04:25 +0300

    update eslint changes

--- sources.js
+++ sources.js
@@ -1,103 +1,104 @@
+/* eslint-disable no-shadow */
 let firstOperand = null;
 let secondOperand = null;
 let operator = null;
 let result = null;
 
-function add(firstOperand,secondOperand) {
-    return firstOperand + secondOperand ;
+function add(firstOperand, secondOperand) {
+  return firstOperand + secondOperand;
 }
 
-
-function extract(firstOperand,secondOperand) {
-    return firstOperand - secondOperand ;
+function extract(firstOperand, secondOperand) {
+  return firstOperand - secondOperand;
 }
 
-
-function multiply(firstOperand,secondOperand) {
-    return firstOperand * secondOperand ;
+function multiply(firstOperand, secondOperand) {
+  return firstOperand * secondOperand;
 }
 
-function divide(firstOperand,secondOperand) {
-    return firstOperand / secondOperand ;
+function divide(firstOperand, secondOperand) {
+  return firstOperand / secondOperand;
 }
 
-function remainder(firstOperand,secondOperand) {
-    return firstOperand % secondOperand 
+function remainder(firstOperand, secondOperand) {
+  return firstOperand % secondOperand;
 }
-
 
 function squareRoot(firstOperand) {
-    return Math.sqrt(firstOperand)
+  return Math.sqrt(firstOperand);
 }
 
-function operate(firstOperand, secondOperand, operator){
-    if (operator === "+"){
-        return add(firstOperand,secondOperand);
-    } else if (operator === "-"){
-        return extract(firstOperand,secondOperand);
-    } else if (operator === "x"){
-        return multiply(firstOperand,secondOperand);
-    } else if (operator === "/"){
-        return divide(firstOperand,secondOperand);
-    } else if (operator === "%"){
-        return remainder(firstOperand,secondOperand);
-    } else {
-        return "Error: invalid operator";
-    }    
+function operate(firstOperand, secondOperand, operator) {
+  if (operator === '+') {
+    return add(firstOperand, secondOperand);
+  }
+  if (operator === '-') {
+    return extract(firstOperand, secondOperand);
+  }
+  if (operator === 'x') {
+    return multiply(firstOperand, secondOperand);
+  }
+  if (operator === '/') {
+    return divide(firstOperand, secondOperand);
+  }
+  if (operator === '%') {
+    return remainder(firstOperand, secondOperand);
+  }
+  return 'Error: invalid operator';
 }
 
-
-const display = document.querySelector("#display")
+const display = document.querySelector('#display');
 
 function updateOperand() {
-    let operandValue = this.value;
-    display.value += operandValue
+  const operandValue = this.value;
+  display.value += operandValue;
 }
-const operand = document.querySelectorAll("#operand")
-operand.forEach(button => {
-    button.addEventListener("click",updateOperand); 
-})
+const operand = document.querySelectorAll('#operand');
+operand.forEach((button) => {
+  button.addEventListener('click', updateOperand);
+});
 
 function updateOperator() {
-    let operatorValue = this.value;
-    if (operatorValue === 'AC'){
-        display.value = ""
-    }  else if (operatorValue === '.'){
-        display.value += operatorValue
-    } else if (operatorValue === '+/-'){
-        display.value = display.value*(-1)
-    } else if (operatorValue === '√x'){
-        display.value = squareRoot(display.value)
-    } else if (operatorValue === '♥'){
-        display.value = ""
-        display.value += operatorValue 
-    } else {
-    firstOperand = display.value
-    display.value = ""
-    display.value += operatorValue
-    operator = display.value    
-    } 
+  const operatorValue = this.value;
+  if (operatorValue === 'AC') {
+    display.value = '';
+  } else if (operatorValue === '.') {
+    display.value += operatorValue;
+  } else if (operatorValue === '+/-') {
+    display.value *= -1;
+  } else if (operatorValue === '√x') {
+    display.value = squareRoot(display.value);
+  } else if (operatorValue === '♥') {
+    display.value = '';
+    display.value += operatorValue;
+  } else {
+    firstOperand = display.value;
+    display.value = '';
+    display.value += operatorValue;
+    operator = display.value;
+  }
 }
-const operators = document.querySelectorAll("#operator");
-operators.forEach(function(operator){
-    operator.addEventListener("click",updateOperator);
-    
-})
+const operators = document.querySelectorAll('#operator');
+operators.forEach((operator) => {
+  operator.addEventListener('click', updateOperator);
+});
 
-
-
-function updateSecondOperand(){
-    const toSliced = display.value
-    if (operator === null || firstOperand === null) {
-        display.value = "";
-        return;
-    }
-    secondOperand = parseFloat(toSliced.slice(1));
-    result = operate(parseFloat(firstOperand), parseFloat(secondOperand), operator);
-    display.value = result;
-    firstOperand = result;
-    operator = null;
+function updateSecondOperand() {
+  const toSliced = display.value;
+  if (operator === null || firstOperand === null) {
+    display.value = '';
+    return;
+  }
+  secondOperand = parseFloat(toSliced.slice(1));
+  result = operate(
+    parseFloat(firstOperand),
+    parseFloat(secondOperand),
+    operator,
+  );
+  display.value = result;
+  firstOperand = result;
+  operator = null;
 }
 
-const equals = document.querySelector("#equal");
-equals.addEventListener("click", updateSecondOperand);
+const equals = document.querySelector('#equal');
+equals.addEventListener('click', updateSecondOperand);
